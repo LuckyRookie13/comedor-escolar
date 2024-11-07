4fa8593377f0488338d19a37c9cdd7ab3b076d13
LuckyRookie13 <diegoronaldjorgeyucra@gmail.com>
2024-11-06 22:06:25 -0300

    Create procesar_reserva.js

---
+++ api/procesar_reserva.js
@@ -0,0 +1,39 @@
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.post('/api/procesar_reserva', (req, res) => {
+  const { nombre, curso, email, horario } = req.body;
+
+  const query = `INSERT INTO reservas (nombre, curso, email, horario) VALUES ($1, $2, $3, $4)`;
+
+  pool.query(query, [nombre, curso, email, horario], (error, results) => {
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+    res.status(200).json({ message: 'Reserva guardada exitosamente!' });
+  });
+});
+
+module.exports = app;
